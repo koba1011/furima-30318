0a42608f1c9c888d7955c69a785744e028300766
kobahumi <mattyaminto@gmail.com>
2020-12-08 00:48:45 +0900

    バリデーション 、エラ〜メッセージ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
+  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
   validates :birthday, presence: true
 end
