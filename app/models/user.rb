217db2ffa62f0d2d82e36570ceedccafefaa0b48
kobahumi <mattyaminto@gmail.com>
2020-12-10 15:56:03 +0900

    バリデーション記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/
+  validates :email, uniqueness: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: true
-    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
     validates :last_name, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
     validates :first_name, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
     validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
