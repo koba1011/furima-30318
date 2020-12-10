2ff8fe49f38f11b13e050532df9d63fb2d86e7e9
kobahumi <mattyaminto@gmail.com>
2020-12-11 01:18:17 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :describe
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_days_id
+    validates :price
+  end
 end
