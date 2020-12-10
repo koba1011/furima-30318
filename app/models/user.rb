3c5cb4deda7654f3ab873202d653bbf367c33402
kobahumi <mattyaminto@gmail.com>
2020-12-10 15:42:20 +0900

    バリデーション 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,14 @@ class User < ApplicationRecord
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/
-  validates :nickname, presence: true
-  validates :email, uniqueness: true
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
-  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
-  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" }
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: "Full-width characters" }
+    validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
+    validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX, message: "Full-width katakana characters" }
+    validates :birthday
+  end
 end
