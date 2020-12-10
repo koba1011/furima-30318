00b6b1b852d1817ba6df36e416d197136d62649e
kobahumi <mattyaminto@gmail.com>
2020-12-10 12:56:45 +0900

    userモデル単体テストコード実装

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.Name}
+    nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
+    password              {"aaaaa1"}
     password_confirmation {password}
     last_name             {"山田"}
     first_name            {"太郎"}
