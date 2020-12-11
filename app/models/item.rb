53dbf010102cb6d5ed2ef1ffd0bd9192b492fb02
kobahumi <mattyaminto@gmail.com>
2020-12-12 00:55:16 +0900

    categoryをビューに表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   
   with_options presence: true do
     validates :image
