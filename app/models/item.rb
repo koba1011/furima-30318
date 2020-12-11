3810c7abe31f6669709641b90417da0536fae08e
kobahumi <mattyaminto@gmail.com>
2020-12-12 01:01:45 +0900

    conditionのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
   
   with_options presence: true do
     validates :image
