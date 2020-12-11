fe63d3a6b0f297c7bb1c2d25895cc77937012401
kobahumi <mattyaminto@gmail.com>
2020-12-12 01:08:35 +0900

    conditionのバリーデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,11 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :describe
-    validates :condition_id
     validates :shipping_fee_id
     validates :prefecture_id
     validates :shipping_days_id
     validates :price
   end
   validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
 end
