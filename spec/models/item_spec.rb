1fda725f2a1e195fd6f5beaa532e64cc74d99a64
kobahumi <mattyaminto@gmail.com>
2020-12-15 16:37:45 +0900

    itemモデルテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe Item, type: :model do
     end
 
     it 'priceの値が300以下の場合は保存できないこと' do
-      @item.price = 1
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include("Price Out of setting range")
     end
