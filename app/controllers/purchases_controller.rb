a8487249709b0d864738baa543a423f5cc5927ce
kobahumi <mattyaminto@gmail.com>
2020-12-22 12:24:50 +0900

    コントローラー修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,23 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @item_purchase = ItemPurchase.new
+  end
+
+  def create
+    @item_purchase = ItemPurchase.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    if @item_purchase.valid?
+      @item_purchase.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:item_purchase).permit(:user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
