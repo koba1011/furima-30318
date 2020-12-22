1bb4336914a835daa1721760b1cdf9f3741affe4
kobahumi <mattyaminto@gmail.com>
2020-12-23 00:35:03 +0900

    決済できるようコントローラー実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,8 +8,9 @@ class PurchasesController < ApplicationController
     @item_purchase = ItemPurchase.new(purchase_params)
     @item = Item.find(params[:item_id])
     if @item_purchase.valid?
+      pay_item
       @item_purchase.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render action: :index
     end
@@ -18,6 +19,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:item_purchase).permit(:user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_purchase).permit(:user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
