6e5e7a3a61d9cbc0a1e632b12ee15195662b652e
kobahumi <mattyaminto@gmail.com>
2020-12-24 00:32:43 +0900

    ファクトリーボットを作成

--- spec/factories/item_purchases.rb
+++ spec/factories/item_purchases.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item_purchase do
-    
+    token         {"tok_abcdefghilk00000000000000000"}
+    postal_code   {"123-4567"}
+    prefecture_id { 2 }
+    city          {"新宿区"}
+    house_number  {"新宿1-1-1"}
+    building_name {"新宿タワー50F"}
+    phone_number  {"09012345678"}
   end
 end
