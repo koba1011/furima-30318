646c44921c8941c87a49a2f4676d23840a81f8eb
kobahumi <mattyaminto@gmail.com>
2020-12-24 16:10:50 +0900

    不要な記述を削除

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -23,7 +23,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:item_purchase).permit(:user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:item_purchase).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
