c532c7db0c7592749978cf4bc49200b1edb09cf0
kobahumi <mattyaminto@gmail.com>
2020-12-28 14:29:32 +0900

    売却済みの商品にはコメントできないようコントローラーを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :soldout_comment, only: :create
   
   def create
     @comment = Comment.new(comment_params)
@@ -17,4 +18,12 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def soldout_comment
+    @item = Item.find(params[:item_id])
+    if @item.purchase.present?
+      redirect_to item_path(@item)
+    end
+  end
 end
+
