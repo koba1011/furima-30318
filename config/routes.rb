801418376791924db18b543fca0e98f3f663c87d
kobahumi <mattyaminto@gmail.com>
2020-12-29 23:53:41 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :items do
     resources :purchases, only: [:index, :create]
     resources :comments, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
