4238904e8fc0aca5e41b38a546efc9244833b9ba
kobahumi <mattyaminto@gmail.com>
2021-01-03 00:27:00 +0900

    コントローラー修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,16 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to new_profile_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:profile_image, :header_image, :text).merge(user_id: current_user.id)
   end
 end
