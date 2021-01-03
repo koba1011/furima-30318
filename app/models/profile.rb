7e0ff3496e7574ba5c8d10462bc89c38076d1722
kobahumi <mattyaminto@gmail.com>
2021-01-04 00:30:14 +0900

    モデル修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_one_attached :profile_image
+  has_one_attached :icon_image
   has_one_attached :header_image
-
-  validates :text, presence: true
-    
 end
