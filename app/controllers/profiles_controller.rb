2edce3e765fb1d1deddb458fcb1e8b7374c0be6f
kobahumi <mattyaminto@gmail.com>
2021-01-04 00:30:35 +0900

    コントローラー修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,38 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    if current_user.profile.present?
+      redirect_to root_path
+    end
     @profile = Profile.new
   end
 
   def create
     @profile = Profile.new(profile_params)
     if @profile.save
-      redirect_to new_profile_path
+      redirect_to user_path(@profile.user.id)
     else
       render :new
     end
   end
 
+  def edit
+    @profile = Profile.find(params[:id])
+    if @profile.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    profile = Profile.find(params[:id])
+    profile.update(profile_params)
+    redirect_to user_path(profile.user.id)
+  end
+
   private
+  
   def profile_params
-    params.require(:profile).permit(:profile_image, :header_image, :text).merge(user_id: current_user.id)
+    params.require(:profile).permit(:icon_image, :header_image, :text).merge(user_id: current_user.id)
   end
 end
