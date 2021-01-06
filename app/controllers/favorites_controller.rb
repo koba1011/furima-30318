f1ae530c484f7b328b739785ef536b1403f5f561
kobahumi <mattyaminto@gmail.com>
2021-01-06 18:22:57 +0900

    お気に入り一覧表示機能作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_favorite
   
   def create
-    @item = Item.find(params[:item_id])
-    if @item.user_id != current_user.id   # 自分の投稿以外にお気に入り登録が可能
+    if @item.user_id != current_user.id
       @favorite = Favorite.create(user_id: current_user.id, item_id: @item.id)
     end
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
     @favorite = Favorite.find_by(user_id: current_user.id, item_id: @item.id)
     @favorite.destroy
+  end
+
+  private
+
+  def set_favorite
+    @item = Item.find(params[:item_id])
   end
 end
