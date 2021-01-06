6a85d4c621558a57d04441e581f49fd566c1db55
kobahumi <mattyaminto@gmail.com>
2021-01-06 17:04:07 +0900

    お気に入り機能作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
+  
   def create
+    @item = Item.find(params[:item_id])
+    if @item.user_id != current_user.id   # 自分の投稿以外にお気に入り登録が可能
+      @favorite = Favorite.create(user_id: current_user.id, item_id: @item.id)
+    end
   end
 
   def destroy
+    @item = Item.find(params[:item_id])
+    @favorite = Favorite.find_by(user_id: current_user.id, item_id: @item.id)
+    @favorite.destroy
   end
 end
