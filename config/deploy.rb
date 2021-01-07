4c5718b8eb8ab99e903354079872aa93bcfcfec5
kobahumi <mattyaminto@gmail.com>
2021-01-08 00:41:48 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'furima-30318'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'koba1011/furima-30318.git'
+set :repo_url,  'git@github.com:koba1011/furima-30318.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
